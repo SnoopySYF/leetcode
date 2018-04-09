f5fe0f478aef806ee43e7e2889679d535cdcac09
MSnoopy <1064632198@qq.com>
2018-04-09 16:26:33 +0800

    没有什么用的优化。。。。

--- leetcode_63.py
+++ leetcode_63.py
@@ -6,14 +6,14 @@ class Solution(object):
         for i in range(m):
             for j in range(n):
                 if(obstacleGrid[i][j] == 1):
-                    grid[i][j] = -99999
+                    grid[i][j] = 0
                 elif(i > 0 and j > 0):
-                    grid[i][j] = max(0, max(0, grid[i - 1][j]) + max(0, grid[i][j - 1]))
+                    grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
                 elif(i == 0 and j > 0):
-                    grid[i][j] = max(0, grid[i][j - 1])
+                    grid[i][j] = grid[i][j - 1]
                 elif(j == 0 and i > 0):
-                    grid[i][j] = max(0, grid[i - 1][j])
-        return max(0, grid[m - 1][n - 1])
+                    grid[i][j] = grid[i - 1][j]
+        return grid[m - 1][n - 1]
 
         
 
