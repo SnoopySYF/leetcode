8538788031bee4dcd0593453adc6ef7c0ec80f69
Yifei.Shuang <1064632198@qq.com>
2019-03-14 16:47:43 +0800

    69

--- leetcode_69.py
+++ leetcode_69.py
@@ -4,11 +4,12 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
+        ##牛顿迭代法
         if x <= 1:
             return x
         r = x
         while r > x / r:
-            r = (r + x / r) / 2
+            r = (r + x / r) // 2   ## // 除法取整数
         return int(r)
 
 print(Solution().mySqrt(7))
